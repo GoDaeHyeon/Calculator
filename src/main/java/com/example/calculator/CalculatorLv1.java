863af2f5712d62897d4d2890cd0bd884b7a0c41f
Daehyeon <tkd6089@naver.com>
2025-01-06 13:44:24 +0900

    add new code

--- src/main/java/com/example/calculator/CalculatorLv1.java
+++ src/main/java/com/example/calculator/CalculatorLv1.java
@@ -11,8 +11,17 @@ public class CalculatorLv1 {
         System.out.println("계산기를 시작합니다. 종료하려면 'exit'를 입력하세요.");
 
         while (true) {
-            // 첫번째 숫자 입력 받기
-            System.out.print("첫 번째 숫자를 이볅하세요.: ");
+            // 첫 번째 숫자 입력 받기
+            System.out.print("첫 번째 숫자를 입력하세요.: ");
+            userInput = sc.next();
+            if (userInput.equals(exitKeyword)) {
+                System.out.print("계산기를 종료합니다.");
+                break;
+            }
+            double num1 = Double.parseDouble(userInput);
+
+            // 두 번째 숫자 입력 받기
+            System.out.print("두 번째 숫자를 입력하세요.: ");
             userInput = sc.next();
             if (userInput.equals(exitKeyword)) {
                 System.out.print("계산기를 종료합니다.");
@@ -22,7 +31,54 @@ public class CalculatorLv1 {
 
             // 연산자 입력받기
             System.out.print("연산자를 입력햐세요 (+, -, *, /): ");
-            userInput
+            userInput = sc.next();
+            if (userInput.equals(exitKeyword)) {
+                System.out.println("계산기를 종료합니다.");
+                break;
+            }
+            char operator = userInput.charAt(0);
+
+            // 연산 수행
+            double result = 0;
+            boolean isValid = true;
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("나눗셈에서 분모는 0이 될 수 없습니다.");
+                        isValid = false;
+                    }
+                    break;
+                default:
+                    System.out.println("잘못된 연산자입니다.");
+                    isValid = false;
+                    break;
+            }
+
+            // 결과 출력
+            if (isValid) {
+                System.out.println("결과: " + result);
+            }
+
+            // 추가 계산 여부 확인
+            System.out.println("더 계산하시겠습니까? (계속하려면 아무 키나 입력 후 enter, 종료하려면 'exit' 입력): ");
+            userInput = sc.next();
+            if(userInput.equals(exitKeyword)) {
+                System.out.println("계산기를 종료합니다.");
+                break;
+            }
         }
+        sc.close();
     }
 }
